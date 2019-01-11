ac5f291e03f8ac122dc3d7ee8fb5058ed8646391
Bar Peled <ibarpeled@gmail.com>
2019-01-11 12:57:49 +0200

    .

---
+++ public/precache-manifest.b23147b0f2523f9500bd7bfac76feacf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ff4f6ba911e7a8bb3efe",
+    "url": "/static/css/main.5646597d.chunk.css"
+  },
+  {
+    "revision": "ff4f6ba911e7a8bb3efe",
+    "url": "/static/js/main.ff4f6ba9.chunk.js"
+  },
+  {
+    "revision": "5fd9828b8e50e2ccaba6",
+    "url": "/static/js/1.5fd9828b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "efe55dd95eb2becad144e6af220e5116",
+    "url": "/static/media/landing.efe55dd9.jpg"
+  },
+  {
+    "revision": "9165b4c769b943159c77e5113f50bf4f",
+    "url": "/index.html"
+  }
+];
